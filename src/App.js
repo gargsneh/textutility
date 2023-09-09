c2444be95b2f851cc6294c22eeb207eeb39feb0a
gargsneh <gargsneh58@gmail.com>
2023-09-09 17:44:52 +0530

    2nd stage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 
 import './App.css';
-let name = "sneh"
+import Navbar from './COMPONENTS/Navbar';
+
 
 function App() {
   return (
-    <>
-    <nav>
-      <li>HOME</li>
-      <li>ABOUT</li>
-      <li>CONTACT</li>
-      <li>EXPLORE</li>
-    </nav>
-    <div className='container'>
-      <h1>HELLO {name} </h1>
-      <p>I know you're eager to start shopping for a car, but first things first—you need to find out how much you can afford to spend.
-You must set priorities and learn how to put first things first.</p>
-    
-    
-    
-    </div>
-    </>
-    
-   
-  );
+<>
+<Navbar title = "Textutils" about= "Aboutus"  />
+</>
+ );
 }
 
 export default App;
