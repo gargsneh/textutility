30af875d903f1a6f0e5c5f35c2f881d4fa717dd3
gargsneh <gargsneh58@gmail.com>
2023-09-09 14:35:56 +0530

    initital commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+let name = "sneh"
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>HOME</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+      <li>EXPLORE</li>
+    </nav>
+    <div className='container'>
+      <h1>HELLO {name} </h1>
+      <p>I know you're eager to start shopping for a car, but first things first—you need to find out how much you can afford to spend.
+You must set priorities and learn how to put first things first.</p>
+    
+    
+    
+    </div>
+    </>
+    
+   
+  );
+}
+
+export default App;
